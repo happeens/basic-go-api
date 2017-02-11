9cc7ced18d3c27480d0e74478485869fb321a7dd
happeens <hilmar.wiegand@flameshome.net>
2017-02-11 12:15:49 +0000

    :construction:

---
+++ app/resource.go
@@ -0,0 +1,25 @@
+package app
+
+import (
+	"bytes"
+	"gopkg.in/gin-gonic/gin.v1"
+
+	. "github.com/happeens/basic-go-api/controllers"
+)
+
+func resource(name string, ctrl Controller, router *gin.Engine) {
+	var buffer bytes.Buffer
+	buffer.WriteString("/")
+	buffer.WriteString(name)
+	buffer.WriteString("s")
+	basePath := buffer.String()
+
+	buffer.WriteString("/:id")
+	idPath := buffer.String()
+
+	router.GET(basePath, ctrl.Index)
+	router.GET(idPath, ctrl.Show)
+	router.POST(basePath, ctrl.Create)
+	router.PUT(idPath, ctrl.Update)
+	router.DELETE(idPath, ctrl.Destroy)
+}
---
+++ app/routes.go
@@ -0,0 +1,15 @@
+package app
+
+import (
+	"gopkg.in/gin-gonic/gin.v1"
+
+	"github.com/happeens/basic-go-api/controllers"
+)
+
+func InitRoutes() *gin.Engine {
+	router := gin.Default()
+
+	resource("todo", controllers.TodoController{}, router)
+
+	return router
+}
